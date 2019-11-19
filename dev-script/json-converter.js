9af7266e4044a996bd8aad87eaa06a462915b7b5
c.masiero <masiero.cristiano@consoft.it>
2019-11-19 17:09:11 +0100

    script for conversion tab1 done!

--- dev-script/json-converter.js
+++ dev-script/json-converter.js
@@ -1,15 +1,13 @@
 var fs = require('fs');
 var path = require('path');
 
-var modelPath = path.dirname(__dirname) + '/data/model/'; 
-var outPath = path.dirname(__dirname) + '/data/'; 
+var modelPath = path.dirname(__dirname) + '/data/model/';
+var outPath = path.dirname(__dirname) + '/data/';
 
 
-fs.readFile(modelPath + 'tab1_response.json', 'utf8', function(err, fileIn) {
+fs.readFile(modelPath + 'tab1_response.json', 'utf8', function (err, fileIn) {
 
-    // console.log(fileIn);
     var jsonIn = JSON.parse(fileIn);
-
     var jsonOut = {};
     // Add request
     jsonOut.request = jsonIn.request;
@@ -17,30 +15,64 @@ fs.readFile(modelPath + 'tab1_response.json', 'utf8', function(err, fileIn) {
     jsonOut.table = [];
 
     // Add table
-    Object.keys(jsonIn.table).forEach(function(tRoot) {
-        
-        var tRootPart = tRoot.split("**");
-        
-        Object.keys(jsonIn.table[tRoot]).forEach(function (tChild) {
-            console.log('[tChild] ' + tChild);
+    for (const tRoot in jsonIn.table) {
+
+        var tRootElements = tRoot.split("**");
+        var tChildElements = [];
+
+        // Builds value array
+        for (const child in jsonIn.table[tRoot]) {
+            var as = child.split("**");
+            var tChild = { "unit": as[0], "data": as[1], "tipo": as[2], "qty": jsonIn.table[tRoot][child] };
+            tChildElements.push(tChild);
+        }
+        /* adds head and values */
+        jsonOut.table.push({
+            "head": { "layer": tRootElements[0], "direzione": tRootElements[1], "mps_rifiuto": tRootElements[2] },
+            "value": tChildElements
         });
-        
-        jsonOut.table.push({ "head":{ "layer":tRootPart[0], "direzione":tRootPart[1], "mps_rifiuto":tRootPart[2] } });
 
-        //console.log('[] ' + tRoot + " " + typeof tRootPart);
-        // add head to table, example  "head" : {"layer" : "FINALI", "direzione" : "MPS", "mps_rifiuto" : "TOTALE"}
-        
-        //jsonOut.table
+    }
 
-        //jsonOut.table.tRootPart.push({"a": "b"});
-        //jsonOut.table.tRootPart = {"a": "b"};
 
-        //console.log('[**] ' + );
-        
-      });
+    /* Adds graph */
+    jsonOut.graph = {};
+    /* Add nodes array */ 
+    jsonOut.graph.nodes = [];
+    for (const key in jsonIn.graph.nodeDict) {
 
-    //console.log('[json-converter] File output');
-    // console.log(JSON.stringify(jsonOut,null,2));
+        var nodeResult = {};
+        nodeResult.name = key;
+        for (const keyInside0 in jsonIn.graph.nodeDict[key]) {
+            const valueInside1 = jsonIn.graph.nodeDict[key][keyInside0];
+            nodeResult[keyInside0] = valueInside1;
+        }
+        jsonOut.graph.nodes.push(nodeResult);
+
+    }
+
+    /* Add links  */
+    jsonOut.graph.links = [];
+    for (const key in jsonIn.graph.edgeDict) {
+        // console.log('[*]' + key);
+        var as = key.split("**");
+        // console.log('[XXX]' + jsonIn.graph.edgeDict[key].tons);
+        jsonOut.graph.links.push({"source": as[0],"target": as[1],"tons": jsonIn.graph.edgeDict[key].tons});
+
+    }
+
+
+    /* Writes output on file */
+    fs.writeFile(outPath + "tab1_response_real.json", JSON.stringify(jsonOut, null, 2), function (err) {
+
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log("The file was saved!");
+    });
+
+
 });
- 
+
 
