a72fc29d4fd1f1bae206055193fd0d58fad72609
c.masiero <masiero.cristiano@consoft.it>
2019-11-28 17:33:56 +0100

    do: fix links to subnode_input

--- test/test-adapter.tab1.js
+++ test/test-adapter.tab1.js
@@ -5,34 +5,72 @@ var fs = require('fs');
 var path = require('path');
 
 const basePath = path.dirname(__dirname);
- 
-const fileJsonInput  = basePath+"\\test\\data\\tab1_response_real.json"; 
-const fileJsonOutput = basePath+"\\test\\data\\out\\tab1_response_real_adapted.json"; 
 
-eval(fs.readFileSync(basePath+"\\lib\\adapter-tab1.js")+'');
+const fileJsonInput = basePath + "\\test\\data\\tab1_response_real.json";
+const fileJsonOutput = basePath + "\\test\\data\\out\\tab1_response_real_adapted.json";
+
+eval(fs.readFileSync(basePath + "\\lib\\adapter-tab1.js") + '');
 
 test('tab1 test', function (t) {
-    
-    t.plan(1);
+
+
 
     // Get file real
     fs.readFile(fileJsonInput, 'utf8', function (err, fileIn) {
 
         //console.log({fileIn});
-        var adp = adapterTab1(JSON.parse(fileIn))
-            .execute();
-        
-        fs.writeFile(fileJsonOutput, JSON.stringify(adp.tab1Adapted, null, 2), function (err) {
+        var adp = adapterTab1(JSON.parse(fileIn)).execute();
+
+        fs.writeFile(fileJsonOutput, JSON.stringify(adp.jsonTab1, null, 2), function (err) {
             // throws an error, you could also catch it here
             if (err) throw err;
-        
+
             // success case, the file was saved
-            console.log('[test-adater.tab1.js] tab1_response_real_adapted.json saved!' );
+            console.log('[test-adater.tab1.js] tab1_response_real_adapted.json saved!');
         });
+
+        var subnodeOutputCount = 0;
+        adp.jsonTab1.graph.nodes.forEach(n => {
+            if (n.subnode_output !== undefined) {
+                subnodeOutputCount += n.subnode_output.length;
+            }
+        });
+
+        var subnodeInputCount = 0;
+        adp.jsonTab1.graph.nodes.forEach(n => {
+            if (n.subnode_input !== undefined) {
+                subnodeInputCount += n.subnode_input.length;
+            }
+        });
+
+        
+        var linkSourceSubnode = adp.jsonTab1.graph.links.filter(l => {
+            return (l.source_subnode !== undefined);
+        });
+
+        var linkTargetSubnode = adp.jsonTab1.graph.links.filter(l => {
+            return (l.target_subnode !== undefined);
+        });
+
+
+        t.plan(10);
+        t.equal(adp.jsonTab1.graph.nodes.length, 19);
+        t.equal(subnodeOutputCount, 19);
+        t.equal(subnodeInputCount, 16);
+        t.equal(linkSourceSubnode.length, 35);
+        t.equal(linkTargetSubnode.length, 25);
+        t.equal(1, 1);
+        t.equal(1, 1);
+        t.equal(1, 1);
+        t.equal(1, 1);
+        t.equal(1, 1);
+        
+
+
 
     });
 
-    t.equal(1,1);
+
 
 
     // t.equal(typeof Date.now, 'function');
